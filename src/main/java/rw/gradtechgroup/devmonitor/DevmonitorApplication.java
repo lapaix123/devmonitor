5e27b3077f49b15ed66db194c289fd3d322b4db0
La-paix <dj.lapaix.tech@gmail.com>
2026-03-16 23:35:28 +0200

    first commit

---
+++ src/main/java/rw/gradtechgroup/devmonitor/DevmonitorApplication.java
@@ -0,0 +1,13 @@
+package rw.gradtechgroup.devmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevmonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rw/gradtechgroup/devmonitor/DevmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.gradtechgroup.devmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
